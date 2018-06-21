d466ebfca5b7b13aaf99daaa9c55c26b9cb554f6
tio-bryan <bryan.pesca@hotmail.com>
2018-06-21 01:57:31 -0300

    criado arquivos do ep2

---
+++ ep2_aed2.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define NUM_CHAVES 2
+#define NUM_FILHOS NUM_CHAVES + 1
+
+typedef struct no {
+    int num_chaves_inserida;
+    int chaves[NUM_CHAVES];
+    struct no * filhos[NUM_FILHOS];
+} No;
+
+int main(int argc, char *argv[]) {
+    //Cria no vazio
+    struct No * raiz = malloc(sizeof(No));
+    for (int i = 0; i < NUM_CHAVES; ++i) {
+        raiz->chaves = NULL;
+    }
+
+    FILE * file = fopen(argv[1], "r");
+    char comando[7];
+    int numero;
+    
+    while (fscanf(file, "%s %i", comando, &numero) != EOF) {
+        if (strcmp(comando, "insere") == 0) {
+            printf("%s %i\n", comando, numero);
+        }
+
+        if (strcmp(comando, "remove") == 0) {
+            printf("%s %i\n", comando, numero);
+        }
+
+        if (strcmp(comando, "imprime") == 0) {
+            printf("%s\n", comando);
+        }
+
+        if (strcmp(comando, "fim") == 0) {
+            printf("%s\n", comando);
+        }   
+    }
+}
