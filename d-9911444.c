6178dfa8cc6c8e58ae6c802333cab2c1f58f993d
tio-bryan <bryan@somarmeteorologia.com.br>
2018-06-21 20:34:30 -0300

    adicionado novas funcoes

--- d-9911444.c
+++ d-9911444.c
@@ -9,20 +9,60 @@ typedef struct no {
     int num_chaves_inserida;
     int chaves[NUM_CHAVES];
     struct no * filhos[NUM_FILHOS];
-} NO;
+} NO_AUX, * NO;
+
+NO noConstructor () {
+    NO aux = malloc(sizeof(NO_AUX));
+    aux->num_chaves_inserida = 0;
+    for (int i = 0; i < NUM_CHAVES; ++i) aux->chaves[i] = -1;
+    for (int i = 0; i < NUM_FILHOS; ++i) aux->filhos[i] = NULL;
+
+    return aux;
+}
+
+int compare(const void* a, const void* b) {
+     int int_a = * ( (int*) a );
+     int int_b = * ( (int*) b );
+
+     if ( int_a == int_b ) return 0;
+     else if ( int_a < int_b ) return -1;
+     else return 1;
+}
 
 int main(int argc, char *argv[]) {
-    //Cria no vazio
-    // NO raiz = malloc(sizeof(NO));
-    // for (int i = 0; i < NUM_CHAVES; ++i) {raiz->chaves = NULL;
+    // Cria no raiz
+    NO raiz = NULL;
 
+    // Abre arquivo
     FILE * file = fopen(argv[1], "r");
     char comando[7];
     int numero;
     
     while (fscanf(file, "%s %i", comando, &numero) != EOF) {
+        // Comando insere
         if (strcmp(comando, "insere") == 0) {
             printf("%s %i\n", comando, numero);
+
+            if (raiz == NULL) {
+                raiz = noConstructor();
+            } else {
+                NO aux = raiz;
+                int inserido = 0;
+
+                while (inserido == 0) {
+                    if (aux->num_chaves_inserida < NUM_CHAVES) {
+                        aux->chaves[aux->num_chaves_inserida] = numero;
+                        qsort(aux->chaves, NUM_CHAVES, sizeof(int), compare);
+                        aux->num_chaves_inserida++;
+                        inserido = 1;
+                    } else {
+                        
+                        inserido = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < NUM_CHAVES; i++) printf("%i\n", raiz->chaves[i]);
         }
 
         if (strcmp(comando, "remove") == 0) {
